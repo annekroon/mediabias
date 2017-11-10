d120fe3e66a6b92c2ce9c64afd65a2a05645f2ac
annekroon <annekroon@gmail.com>
2017-11-10 14:40:41 +0100

    extended query; save model

--- voorbeeldscript.py
+++ voorbeeldscript.py
@@ -26,13 +26,26 @@ def example_nu():
 class example_telegraaf():
 
     def __init__(self):
-        docs = inca.core.database.scroll_query({
-        "query":{
-            "match":{
-                "doctype": "telegraaf (www)"
+        #docs = inca.core.database.scroll_query({
+        #"query":{
+        #    "match":{
+        #        "doctype": "telegraaf (www)"
+        #        }
+        #}
+        #})
+
+        docs = inca.core.database.scroll_query(
+            {
+                  "query": {
+                          "bool": {
+                                    "filter": [
+                                                { "match": { "doctype": "telegraaf (print)" }},
+                                                { "range": { "publication_date": { "gte": "2014-01-01", "lt":"2014-02-01" }}}
+                                              ]
+                                  }
+                        }
                 }
-        }
-    })
+            )
         
         self.sentences = []
         n=0
@@ -51,6 +64,7 @@ class example_telegraaf():
 
         self.model = gensim.models.Word2Vec(self.sentences, min_count=1)
 
+        print('Estimated Word2Vec model')
 
 
     
@@ -59,4 +73,7 @@ if __name__ == "__main__":
     casus = example_telegraaf()
     m0 = casus.model
     print(m0.most_similar(positive=['buitenlandse'], negative=['media']))
-
+    with open('/home/anne/word2vec_telegraaf2maanden', mode='wb') as fo: 
+        m0.save(fo)
+    print('Saved model')
+    print("reopen it with m = gensim.models.Word2Vec.load('/home/anne/word2vec_telegraaf2maanden'")
