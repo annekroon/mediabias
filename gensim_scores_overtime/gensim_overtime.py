12c037b4363d763417331171986ff409156142b8
annekroon <annekroon@gmail.com>
2018-01-11 11:37:42 +0100

    gensim scores overtime

--- gensim_scores_overtime/gensim_overtime.py
+++ gensim_scores_overtime/gensim_overtime.py
@@ -9,64 +9,58 @@ import json
 import itertools
 print(sys.executable)
 print(sys.version)
+import pandas as pd
 
-basepath = "../trained_models/test/"
+basepath = "/Users/admin/trained_data/tel"
 
-class get_word2vec():
+class word2vec_analyzer():
+    '''This class provides a Bias Analyzer for Word2Vec models.'''
 
     def __init__(self):
-
-        self.doctype = []
-        self.fromdate = []
-        self.todate = []
         self.nmodel = 0
         self.combinations_crime = []
         self.combinations_low = []
-        with open('ressources/combinations_crime.csv') as fi:
+        with open('../ressources/combinations_crime.csv') as fi:
             for line in fi.readlines():
                 self.combinations_crime.append(tuple(line.strip().split(',')))
-        with open('ressources/combinations_lowlife.csv') as fi:
+        with open('../ressources/combinations_lowlife.csv') as fi:
             for line in fi.readlines():
                 self.combinations_low.append(tuple(line.strip().split(',')))
+        logger.info("Created analyzer with {} combinations for crime and {} combinations for low life".format(
+            len(self.combinations_crime), len(self.combinations_low)))
+
+
+    def get_model(self):
+        '''yields a dict with one item. key is the filename, value the gensim model'''
+
         for fname in os.listdir(basepath):
+            model = {}
             path = os.path.join(basepath, fname)
-        try:
-            splitResult = fname.split( " - " ) #split on scores
-            self.doctype.append(splitResult[1])
-            self.fromdate.append(splitResult[2])
-            self.todate.append(splitResult[3])
-        except IndexError:
-            pass
-
-    def get_models(self):
-
-        ''' calling models'''
-
-        models = {}
-        for fname in os.listdir(basepath):
-            path = os.path.join(basepath, fname)
-            models['fname'] = gensim.models.Word2Vec.load(path)
+            model['gensimmodel'] = gensim.models.Word2Vec.load(path)
+            model['filename'] = fname
+            splitResult = fname.split( "_" ) #split on scores
+            model['doctype'] = splitResult[1]
+            model['fromdate'] = splitResult[2]
+            model['todate'] = splitResult[3]
             self.nmodel +=1
-        yield models
+            yield model
 
     def get_scores(self):
-
-        ''' calling similarity scores'''
-
         results = []
-        for x in self.get_models():
+        for model in self.get_model():
+            logger.info("Retrieving similarity scores from model {} ...".format(model['filename']))
             for pair in self.combinations_crime:
                 try:
-                    r = x['fname'].wv.similarity(pair[0],pair[1])
+                    r = model['gensimmodel'].wv.similarity(pair[0],pair[1])
                     pair_1 = pair[0]
                     pair_2 = pair[1]
                 except KeyError:
                     r = 0
                 results.append({'pair': (pair[0],pair[1]),
                             'word2vec_score': r,
-                            'doctype': self.doctype,
-                            'fromdate': self.fromdate,
-                            'todate': self.todate})
+                            'doctype': model['doctype'],
+                            'fromdate': model['fromdate'],
+                            'todate': model['todate']})
         return results
 
 
@@ -76,15 +70,22 @@ if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
     logging.root.setLevel(level=logging.INFO)
 
+    print("\n\n\nInitiate Analyzer for my dataset\n\n\n")
+    myanalyzer = word2vec_analyzer()
+    print("\n\n\nCalculate bias scores\n\n\n")
+    gensimscore = myanalyzer.get_scores()
 
-r = get_word2vec()
-gensimscore = r.get_scores()
+    print("\n\n\nSave results\n\n\n")
+    with open('../gensim_scores_overtime/output_tel.json',mode='w') as fo:
+        fo.write('[')
 
-with open('gensim_scores_overtime/output.json',mode='w') as fo:
-    fo.write('[')
+        for result in gensimscore:
+            #print("this is the result:", result)
+            fo.write(json.dumps(result))
+            fo.write(',\n')
+        fo.write('[]]')
 
-    for result in gensimscore:
-        print("this is the result:", result)
-        fo.write(json.dumps(result))
-        fo.write(',\n')
-    fo.write('[]]')
+    df = pd.DataFrame.from_dict(gensimscore)
+    print('Created dataframe')
+    # print(df)
+    df.to_csv('output_tel.csv')
