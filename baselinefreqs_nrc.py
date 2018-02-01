a9b892c0fedbfb52d4a8833133fe0806a0a407e9
annekroon <annekroon@gmail.com>
2018-02-01 14:29:57 +0100

    added baseline freqs

---
+++ baselinefreqs_nrc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import inca
+words = [w.strip() for w in open('ressources/words_target.txt').readlines() if len(w.strip())>0]
+
+
+
+
+fromdate = '2000-01-01'
+todate = '2017-12-31'
+listofdoctypes = 'nrc (print)'
+
+
+analyzer = inca.analysis.timeline_analysis.timeline_generator()
+
+for word in words:
+    print('Now counting articles containing "{}"'.format(word))
+    q = {"query": {
+            "bool": {
+                           "must": [{"query_string" : {"query" : word}}],
+                        "filter": [ {'bool': {'should': [{ "match": { "doctype.keyword": d}} for d in listofdoctypes]}},
+                                          { "range": { "publication_date": { "gte": fromdate, "lt":todate }}}]}
+            }}
+
+    doctypequery = "doctype:"+" OR doctype:".join(['"{}"'.format(d) for d in listofdoctypes])
+    qs  = '{} AND ({})' .format(word,doctypequery)
+    print(qs)
+    
+    df = analyzer.analyse(queries = qs, granularity='year',timefield='publication_date',from_time='1991-01-01')
+    df.to_csv('output/baselinecount-{}.csv'.format(word))
